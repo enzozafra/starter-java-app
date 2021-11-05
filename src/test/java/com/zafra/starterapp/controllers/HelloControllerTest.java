0570d0390a54c713b57aea1f31bb7e66806844bd
Enzo Zafra <enzo@squareup.com>
2021-11-04 17:48:36 -0700

    add readme

--- src/test/java/com/zafra/starterapp/controllers/HelloControllerTest.java
+++ src/test/java/com/zafra/starterapp/controllers/HelloControllerTest.java
@@ -1,15 +1,11 @@
 package com.zafra.starterapp.controllers;
 
 import static org.hamcrest.Matchers.equalTo;
-import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import com.zafra.starterapp.handlers.Handler;
 import org.junit.jupiter.api.Test;
 
-import org.junit.runner.RunWith;
-import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
